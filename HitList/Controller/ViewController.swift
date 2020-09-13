d8062aac3915806edfa3c8b66b72c86d3666cbcf
Armando Carrillo <ingcompuarmando@gmail.com>
2020-09-13 15:52:34 -0500

    Fetching from Core Dataa

--- HitList/Controller/ViewController.swift
+++ HitList/Controller/ViewController.swift
@@ -34,9 +34,29 @@ class ViewController: UIViewController {
     
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        title = "the List"
+        
+        title = "The List"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+    
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName : "Person")
+        
+        do {
+            people = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
     }
 
     @IBAction func AddName(_ sender: UIBarButtonItem) {
